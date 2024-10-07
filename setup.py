d87ec22f1a5a0529a9ea3fe71328e11b4c2fb5a8
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-10-07 02:46:19 -0500

    Include package data

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     version='0.1',  # You can set your own version here.
     packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
+    package_data={
+        'openalex_http.legacy': ['data/*']
+    },
+    include_package_data=True,
     url='https://github.com/ourresearch/openalex-http',
     license='',
     author='',
